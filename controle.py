3b2ec85e25fe3bbf9b4423d7e22985cc817db823
Allex Magno <allex.m@aluno.ifsc.edu.br>
2018-09-13 23:07:37 -0300

    feat: Controle de testes

--- controle.py
+++ controle.py
@@ -1,40 +1,62 @@
 from autonomo import *
-
 import threading
 
 robot = Movimento('outA', 'outD', 200)
 
-c1 = Coordenadas(0,0,'L')
-c2 = Coordenadas(0,0,'L')
-c3 = Coordenadas(6,6,'O')
+c1 = Coordenadas(0, 0, 'L')
+c2 = Coordenadas(0, 0, 'L')
+c3 = Coordenadas(6, 6, 'O')
 s = []
-aut = Autonomo(c1,c2,c3,s,robot)
 
+aut = Autonomo(c1, c2, c3, s, robot)
+
+
+def setAutonomo():
+    s = int(input("1 (Ne), 2 (Se), 3 (No), 4 (Ne)"))
+    t1 = int(input("x: "))
+    t2 = int(input("y: "))
+    t3 = int(input("nav: "))
+
+    if s == 1:
+        aut.setNe(t1, t2, t3)
+    elif s == 2:
+        aut.setNe(t1, t2, t3)
+    elif s == 3:
+        aut.setNe(t1, t2, t3)
+    elif s == 4:
+        aut.setNe(t1, t2, t3)
+
+    print(aut.getCoordenada())
+
+
+def setManual():
+    print(
+        "selecione um movimento:\n a - esquerda\n d - direita\n w - seguir em frente\n s - Retornar \n space - Parar \n")
+    op = input("digite uma opcao:")
+
+    if op == "a":
+        a =threading.Thread(target=robot.setEsquerda)
+        a.start()
+
+    if op == "d":
+        d = threading.Thread(target=robot.setDireita)
+        d.start()
+    if op == "w":
+        w = threading.Thread(target=robot.setFrente)
+        w.start()
+
+    if op == "s":
+        s = threading.Thread(target=robot.setRetornar)
+        s.start()
+
+    if op == chr(32):
+        spc = threading.Thread(target=robot.setParar)
+        spc.start()
 
 
 while (True):
-    a = input("(1) Manual \n(2) Autonomo:")
-    if a == "1":
-        print("selecione um movimento:\n a - esquerda\n d - direita\n w - seguir em frente\n s - Parar \n")
-        op = input("digite uma opcao:")
-
-        if op == "a":
-            robot.setParar()
-            robot.setEsquerda()
-
-        if op == "d":
-            robot.setParar()
-            t1 = threading.Thread(target=robot.setDireita())
-
-        if op == "w":
-            t2 = threading.Thread(target=robot.setFrente())
-
-        if op == "s":
-            t3 = threading.Thread(robot.setRetornar())
+    a = input("(1) Autono \n(2) Manual:")
     if a == "2":
-        t1 = int(input("x"))
-        t2 = int(input("y"))
-        t3 = int(input("nav"))
-        aut.setNordeste(t1,t2,t3)
-        print(aut.getCoordenada())
-
+        setManual()
+    elif a == "1":
+        setAutonomo()
--- dev.py
+++ dev.py
@@ -1,12 +1,87 @@
-from autonomo import *
+import threading
+from time import sleep
+
+def setDireita():
+    i = 0
+    while i < 5:
+        print("direita")
+        i = i + 1
+        sleep(0.5)
+
+def setEsquerda():
+    i = 0
+    while i < 5:
+        print("esquerda")
+        i = i + 1
+        sleep(0.5)
+
+def setFrente():
+    i = 0
+    while i < 5:
+        print("Frente")
+        i = i + 1
+        sleep(0.5)
+
+def setRetornar():
+    i = 0
+    while i < 5:
+        print("retornar")
+        i = i + 1
+        sleep(0.5)
+
+def setParar():
+    i = 0
+    while i < 5:
+        print("parar")
+        i = i + 1
+        sleep(0.5)
 
 
-#robot = Movimento('outA', 'outD', 200)
-robot = "a"
-c1 = Coordenadas(1,2,'N')
-c2 = Coordenadas(0,0,'L')
-c3 = Coordenadas(6,6,'O')
-s = []
-aut = Autonomo(c1,c2,c3,s,robot)
-print(aut.getCoordenada())
-print(c1.toString())
+def setAutonomo():
+    s = int(input("1 (Ne), 2 (Se), 3 (No), 4 (Ne)"))
+    t1 = int(input("x: "))
+    t2 = int(input("y: "))
+    t3 = int(input("nav: "))
+
+    if s == 1:
+        print("s = 1")
+    elif s == 2:
+        print("s = 2")
+    elif s == 3:
+        print("s = 3")
+    elif s == 4:
+        print("s = 4")
+
+
+
+def setManual():
+    print(
+        "selecione um movimento:\n a - esquerda\n d - direita\n w - seguir em frente\n s - Retornar \n space - Parar \n")
+    op = input("digite uma opcao:")
+
+    if op == "a":
+        a = threading.Thread(target=setEsquerda)
+        a.start()
+
+    if op == "d":
+        d = threading.Thread(target=setDireita)
+        d.start()
+    if op == "w":
+        w = threading.Thread(target=setFrente)
+        w.start()
+
+    if op == "s":
+        s = threading.Thread(target=setRetornar)
+        s.start()
+
+    if op == chr(32):
+        spc = threading.Thread(target=setParar)
+        spc.start()
+
+
+while (True):
+    a = input("(1) Autonomo \n(2) Manual:")
+    if a == "2":
+        setManual()
+    elif a == "1":
+        setAutonomo()
