4ac99a29349987ff3b56695654f4552744c32d5a
Allex Magno <allex.m@aluno.ifsc.edu.br>
2018-09-22 15:25:57 -0300

    debug: controle

--- controle.py
+++ controle.py
@@ -27,62 +27,52 @@ def setNavegacao():
     elif s == 4:
         nav.setSo(t1, t2, t3)
 
-
-def setManual():
-    print(
-        "selecione um movimento:\n a - esquerda\n d - direita\n w - seguir em frente\n s - Retornar \n space - Parar \n")
-    op = input("digite uma opcao:")
-
-    if op == "a":
-        robot.setEsquerda()
-
-    if op == "d":
-        robot.setDireita()
-
-    if op == "w":
-        robot.setFrente()
-
-    if op == "s":
-        robot.setRetornar()
-
-    if op == chr(32):
-        robot.setParar()
-
 def exe():
     print(aut.executar())
 
 c1 = Coordenadas(0, 0, "L")
 c2 = Coordenadas(0, 0, "L")
 c3 = Coordenadas(6, 6, 'O')
-s = [Coordenadas(0,6," "),Coordenadas(2,1," "),Coordenadas(4,0," "),Coordenadas(6,1," "),Coordenadas(3,5," "), Coordenadas(5,5, " "), Coordenadas(2,2, " "), Coordenadas(1,0, " "), Coordenadas(6,6, " ")]
+c4 = Coordenadas(0, 6, ' ')
+c5 = Coordenadas(3, 3, ' ')
+c6 = Coordenadas(6, 0, ' ')
+c7 = Coordenadas(1, 1, ' ')
+c8 = Coordenadas(2, 2, ' ')
+c9 = Coordenadas(5, 6, ' ')
+
+
+s = [c4, c5, c6, c7, c8, c9]
 aut = Autonomo(c1, c2, c3, s, robot)
-nav = Navegacao(robot,c1)
-while (True):
-    if colors[sc.value()] == "yellow":
-        dir = input("Posicao inicial (0,0). informe direcao: ")
-        c1 = Coordenadas(0, 0, dir)
+nav = Navegacao(robot, c1)
 
-    if colors[sc.value()] == "blue":
-        dir = input("Posicao inicial (6,6). informe direcao: ")
-        c1 = Coordenadas(6, 6, dir)
-
-
-    print("Posicao atual: " + nav.getCoordenada())
-    corrigir = input("Corrigir posicao? s ou n: ")
-    if corrigir == "s":
-        x = int(input("X atual: "))
-        y = int(input("Y atual: "))
-        dir = input("Direcao: ")
-        print("Posicao atual ({},{},{})".format(x,y,dir))
-        c1 = Coordenadas(x, y, dir)
-        aut.setCoordenada(c1)
-
-    a = input("(1) Autono \n(2) Manual: \n(3) Autonomo.exe")
+while True:
+    a = input("(1) Autonomo \n(2) Manual: \n(3) Autonomo.exe")
     if a == "2":
-        setManual()
+       manual.comandos()
 
-    elif a == "1":
-        setAutonomo()
+    if a == 1:
+        if colors[sc.value()] == "yellow":
+            dir = input("Posicao inicial (0,0). informe direcao: ")
+            c1 = Coordenadas(0, 0, dir)
 
-    elif a == "3":
-        exe()
+        if colors[sc.value()] == "blue":
+            dir = input("Posicao inicial (6,6). informe direcao: ")
+            c1 = Coordenadas(6, 6, dir)
+
+
+        print("Posicao atual: " + nav.getCoordenada())
+
+        corrigir = input("Corrigir posicao? s ou n: ")
+        if corrigir == "s":
+            x = int(input("X atual: "))
+            y = int(input("Y atual: "))
+            dir = input("Direcao: ")
+            print("Posicao atual ({},{},{})".format(x,y,dir))
+            c1 = Coordenadas(x, y, dir)
+            aut.setCoordenada(c1)
+
+        b = input("(1) Autonomo \n(3) Autonomo \n")
+        if b == 1:
+            setNavegacao()
+        elif b == 2:
+            exe()
