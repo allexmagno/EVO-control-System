8d9b56401115c17e5ad9085e217d23bcfe396e10
DouglasAmorim <amorimdos@gmail.com>
2018-09-21 21:48:10 -0300

    Alteracao executar/estrategia/classeQuadrante

---
+++ quadrante.py
@@ -0,0 +1,42 @@
+
+
+class quadrante():
+    def __init__(self,quadranteInicial,sequencia):
+        self.quadranteInicial = quadranteInicial
+        self.listSeq = sequencia
+        self.quadA = []
+        self.quadB = []
+        self.quadC = []
+        self.quadIgnorado = []
+
+    def dividirCacas(self):
+        i = 0
+        while i < len(self.sequencia):
+            if(self.quadranteInicial == 0):
+                if((self.sequencia[i].getX() <= 2 and self.sequencia[i].getY() <= 2) or (self.sequencia[i].getX() == 3 and self.sequencia[i].get3() == 3) ):
+                    self.quadA[i] = self.sequencia[i]
+                elif(self.sequencia[i].getX() >= 3 and self.sequencia[i].getY() <= 2):
+                    if((self.sequencia[i].getX() == 5 and self.sequencia[i].getY() == 2) or (self.sequencia[i].getX() == 6 and self.sequencia[i].getY() == 2)):
+                        self.quadIgnorado[i] = self.sequencia[i]
+                    self.quadB[i] = self.sequencia[i]
+                elif(self.sequencia[i].getX() <= 2 and self.sequencia[i].getY() >= 3):
+                    if((self.sequencia[i].getX() == 2 and self.sequencia[i].getY() == 5) or (self.sequencia[i].getX() == 2 and self.sequencia[i].getY() == 6)):
+                        self.quadIgnorado[i] = self.sequencia[i]
+                    self.quadC[i] = self.sequencia[i]
+                else:
+                    self.quadIgnorado[i] = self.sequencia[i]
+
+            if(self.quadranteInicial == 1):
+                if((self.sequencia[i].getX() >= 4 and self.sequencia[i].getY() >= 4) or (self.sequencia[i].getX() == 3 and self.sequencia[i].get3() == 3) ):
+                    self.quadA[i] = self.sequencia[i]
+                elif(self.sequencia[i].getX() >= 4 and self.sequencia[i].getY() <= 3):
+                    if((self.sequencia[i].getX() == 4 and self.sequencia[i].getY() == 0) or (self.sequencia[i].getX() == 4 and self.sequencia[i].getY() == 1)):
+                        self.quadIgnorado[i] = self.sequencia[i]
+                    self.quadB[i] = self.sequencia[i]
+                elif(self.sequencia[i].getX() <= 3 and self.sequencia[i].getY() >= 4):
+                    if((self.sequencia[i].getX() == 0 and self.sequencia[i].getY() == 4) or (self.sequencia[i].getX() == 1 and self.sequencia[i].getY() == 4)):
+                        self.quadIgnorado[i] = self.sequencia[i]
+                    self.quadC[i] = self.sequencia[i]
+                else:
+                    self.quadIgnorado[i] = self.sequencia[i]
+            i = i + 1
